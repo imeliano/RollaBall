2d68baa550a2706df30e4e0700b7b85b51ab5e53
Ime-liano <epolito2010@gmail.com>
2024-09-24 19:07:35 -0700

    Changes to Portal DOTween

--- Assets/Scripts/TweenPratice.cs
+++ Assets/Scripts/TweenPratice.cs
@@ -9,20 +9,22 @@ public class TweenPratice : MonoBehaviour
     public Vector3[] Pathwaypoints;
 
     // Start is called before the first frame update
+
     void Start()
     {
         //DOTween.To(()=> transform.position.y,x=> transform.position = new Vector3(transform.position.x, x, transform.position.z), 50, 1). SetEase(Ease.myAnimationCurve);
         //transform.DOBlendableMoveBy(new Vector3(-3, 3, 0), 2);
-
-
-        transform.DOLocalPath(Pathwaypoints, 2f, PathType.CatmullRom);
         //﻿﻿﻿﻿﻿DOTween.RewindAll();
-        
+        transform.DOLocalPath(Pathwaypoints, 2f, PathType.CatmullRom).SetLoops(-1);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //new Vector3 = Vector3.angle(transform.forward, refrence);
+        
+
         //transform.position = new Vector3 (Mathf.Cos(Time.deltaTime), Mathf.Sin(Time.deltaTime), 0);
     }
 }
